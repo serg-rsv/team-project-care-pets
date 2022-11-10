19d12e6519c4c013633079f3751e8921cce4b143
Mykola Tymoshchuk <saintnikfreak@gmail.com>
2022-11-10 14:40:14 +0200

    issue #1 add miins

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Care Pets</h1>
+        <h1>Hello, we are from Ukraine</h1>
+        <h2>Русский военный корабль, ИНХ</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
