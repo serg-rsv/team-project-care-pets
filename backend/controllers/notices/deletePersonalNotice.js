51d1c072f458d2bbef26afa80d0d9b4a04678880
Serhii Rybak <rybaksw@gmail.com>
2022-11-12 18:15:27 +0200

    feat: clear favorites after notice remove

--- backend/controllers/notices/deletePersonalNotice.js
+++ backend/controllers/notices/deletePersonalNotice.js
@@ -1,12 +1,23 @@
 const asyncHandler = require('express-async-handler');
 
-const { Notice } = require('../../models');
+const { Notice, User } = require('../../models');
 const { RequestError } = require('../../helpers');
 
 const deletePersonalNotice = asyncHandler(async (req, res) => {
   const { noticeId } = req.params;
 
   // Зачистити ІД в обраних серед юзерів ?
+  const users = await User.find({ favorites: noticeId });
+  users.forEach(async user => {
+    const idx = user.favorites.indexOf(noticeId);
+    if (idx === -1) {
+      throw RequestError(400, 'Favorite is not found');
+    }
+
+    user.favorites.splice(idx, 1);
+    await user.save();
+  });
+
   const isRemoved = await Notice.findByIdAndDelete(noticeId);
 
   if (!isRemoved) {
--- frontend/src/components/PetPage/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ frontend/src/components/PetPage/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -1,43 +1,33 @@
 import s from './NoticeCategoryItem.module.scss';
 
-
-
-const NoticeCategoryItem = ({
-link, 
-title,
-breed,
-place,
-age,
-price
-}) => {
-return (
+const NoticeCategoryItem = ({ link, title, breed, place, age, price }) => {
+  return (
     <li className={s.animalListItem}>
-    <div className={s.signature}>
-       <p>In good hands</p>
-    </div>
-    <img className={s.animalListImg} src={link} alt={title}/>
-    <h3 className={s.animalListTitle}>{title}</h3>
-    <div className={s.animalListBoxText}>
-    <p className={s.breed}>Breed:</p>
-    <p>{breed}</p>
-    </div>
-    <div className={s.animalListBoxText}>
-    <p className={s.place}>Place:</p>
-    <p>{place}</p>
-    </div>
-    <div className={s.animalListBoxText}>
-    <p className={s.age}>Age:</p>
-    <p>{age}</p>
-    </div>
-    {price && <div className={s.animalListBoxText}>
-    <p className={s.price}>Price:</p>
-    <p>{price}</p></div>}
-    
-    
+      <div className={s.signature}>
+        <p>In good hands</p>
+      </div>
+      <img className={s.animalListImg} src={link} alt={title} />
+      <h3 className={s.animalListTitle}>{title}</h3>
+      <div className={s.animalListBoxText}>
+        <p className={s.breed}>Breed:</p>
+        <p>{breed}</p>
+      </div>
+      <div className={s.animalListBoxText}>
+        <p className={s.place}>Place:</p>
+        <p>{place}</p>
+      </div>
+      <div className={s.animalListBoxText}>
+        <p className={s.age}>Age:</p>
+        <p>{age}</p>
+      </div>
+      {price && (
+        <div className={s.animalListBoxText}>
+          <p className={s.price}>Price:</p>
+          <p>{price}</p>
+        </div>
+      )}
     </li>
-   
-)
-
+  );
 };
 
 export default NoticeCategoryItem;
