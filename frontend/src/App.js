869f041548f9f1e3019ce8fc1ca234aed894dd6d
Oleksii Skladaniuk <skladanyuk1999@gmail.com>
2022-11-15 12:20:13 +0200

    Виправив роут та добавив спінер

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,22 @@ import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import PrivateRoute from './components/Routes/PrivateRoute';
 import PublicRoute from './components/Routes/PublicRoute';
+import  {Loader}  from './components/Loader/Loader';
 
-const Header = lazy(() => import('./components/Header')); // Хедер потрібно поміняти на Sharedlayout 
+const Header = lazy(() => import('./components/Header')); // Хедер потрібно поміняти на Sharedlayout
 const RegisterPage = lazy(() => import('./pages/RegisterPage'));
 const LoginPage = lazy(() => import('./pages/LoginPage'));
-const NewsPage = lazy(() => import('./pages/NewsPage'))
+const NewsPage = lazy(() => import('./pages/NewsPage'));
 const OurFriendsPage = lazy(() => import('./pages/OurFriendsPage'));
 const NoticesPage = lazy(() => import('./pages/NoticesPage'));
-const UserPage = lazy(() => import('./pages/UserPage')) 
-
+const UserPage = lazy(() => import('./pages/UserPage'));
 
 function App() {
   return (
-    <Suspense>
+    <Suspense fallback={<Loader/>}>
       <Routes>
-        <Route path="/" element={<Header />}>  
-          <Route element={<PublicRoute component={<RegisterPage />} />}>
+        <Route path="/" element={<Header />}>
+          <Route element={<PublicRoute restricted />}>
             <Route path="/register" element={<RegisterPage />} />
           </Route>
           <Route element={<PublicRoute restricted />}>
