d0dbf7f09f874eb2ecd1294a60971e6b4cb83a1d
Serhii Rybak <rybaksw@gmail.com>
2022-11-17 01:20:36 +0200

    Update store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { noticesApi } from './services/noticesSlice';
 import { petsApi } from './services/petsSlice';
+import { usersApi } from './usersSlice';
 export const store = configureStore({
   reducer: {
     [noticesApi.reducerPath]: noticesApi.reducer,
