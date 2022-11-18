149129d0056eb4caccf102a13cff28e3abb71eda
Serhii Rybak <rybaksw@gmail.com>
2022-11-19 00:14:12 +0200

    feat: logout

--- frontend/src/components/LogOut/LogOut.jsx
+++ frontend/src/components/LogOut/LogOut.jsx
@@ -1,8 +1,23 @@
+import { useDispatch } from 'react-redux';
+import { unsetToken } from '../../redux/services/authSlice';
+import { useLogoutMutation } from '../../redux/services/usersSlice';
 import Button from '../Button/Button';
 import scss from './LogOut.module.scss';
+
+import { noticesApi } from '../../redux/services/noticesSlice';
+import { petsApi } from '../../redux/services/petsSlice';
+import { usersApi } from '../../redux/services/usersSlice';
+
 const LogOut = () => {
-  const logOut = () => {
-    console.log('Log out'); // вихід з облікового запису
+  const dispatch = useDispatch();
+  const [logout] = useLogoutMutation();
+
+  const logOut = async () => {
+    await logout();
+    dispatch(unsetToken());
+    dispatch(noticesApi.util.resetApiState());
+    dispatch(petsApi.util.resetApiState());
+    dispatch(usersApi.util.resetApiState());
   };
   return (
     <Button onClick={logOut} className={scss.logOutBtn}>
