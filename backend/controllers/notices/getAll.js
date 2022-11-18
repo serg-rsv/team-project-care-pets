57a05568a0328ff3c9159093a4d92c6047cb6efe
Mykola Tymoshchuk <saintnikfreak@gmail.com>
2022-11-18 12:06:31 +0200

    add filter and pagination to notice/getAll controller

--- backend/controllers/notices/getAll.js
+++ backend/controllers/notices/getAll.js
@@ -4,10 +4,33 @@ const { Notice } = require('../../models');
 const { RequestError } = require('../../helpers');
 
 const getAll = asyncHandler(async (req, res) => {
-  const notices = await Notice.find({}, '-createdAt -updatedAt').populate(
-    'owner',
-    'email phone'
-  );
+  const {
+    title = '',
+    category = '',
+    location = '',
+    name = '',
+    owner = '',
+    page = 1,
+    limit = 10,
+  } = queries;
+  const skip = (page - 1) * limit;
+
+  const notices = req.query
+    ? await Notice.find(
+        {
+          title: { $regex: new RegExp(title, 'i') },
+          category: { $regex: new RegExp(category, 'i') },
+          location: { $regex: new RegExp(location, 'i') },
+          name: { $regex: new RegExp(name, 'i') },
+          owner: { _id: owner },
+        },
+        { skip, limit },
+        '-createdAt -updatedAt'
+      ).populate('owner', 'email phone')
+    : await Notice.find({}, { skip, limit }, '-createdAt -updatedAt').populate(
+        'owner',
+        'email phone'
+      );
 
   if (!notices) {
     throw RequestError(404, 'Not found');
