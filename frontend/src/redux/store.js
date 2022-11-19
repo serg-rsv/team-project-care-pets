304f23a86feeb136f8ed3c47ebb91e44d3588aa9
Kaplynenko <93135352+Kaplynenko@users.noreply.github.com>
2022-11-19 22:17:26 +0200

    fix

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,19 +3,37 @@ import { noticesApi } from './services/noticesSlice';
 import { petsApi } from './services/petsSlice';
 import { usersApi } from './services/usersSlice';
 import authReducer from './services/authSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token', 'isLoggedin'],
+};
+const persistedUserReducer = persistReducer(authPersistConfig, authReducer);
 export const store = configureStore({
   reducer: {
     [noticesApi.reducerPath]: noticesApi.reducer,
     [petsApi.reducerPath]: petsApi.reducer,
-    auth: authReducer,
+    auth: persistedUserReducer,
 
     [usersApi.reducerPath]: usersApi.reducer,
   },
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(
-      noticesApi.middleware,
-      petsApi.middleware,
-      usersApi.middleware
-    ),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(noticesApi.middleware, petsApi.middleware, usersApi.middleware),
 });
+export const persistor = persistStore(store);
