074d2144c9f6e533f1a4f5aba0bc7c1b377fb553
Serhii Rybak <rybaksw@gmail.com>
2022-11-26 13:09:41 +0200

    Update NoticeCategoryItem.jsx

--- frontend/src/components/PetPage/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ frontend/src/components/PetPage/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -94,14 +94,14 @@ const NoticeCategoryItem = ({
           <path
             d="M7 1C3.6868 1 1 3.73373 1 7.10648C1 9.8291 2.05 16.2909 12.3856 22.8229C12.5707 22.9387 12.7833 23 13 23C13.2167 23 13.4293 22.9387 13.6144 22.8229C23.95 16.2909 25 9.8291 25 7.10648C25 3.73373 22.3132 1 19 1C15.6868 1 13 4.70089 13 4.70089C13 4.70089 10.3132 1 7 1Z"
             fill="white"
-            fill-opacity="0.6"
+            fillOpacity="0.6"
           />
           <path
             d="M7 1C3.6868 1 1 3.73373 1 7.10648C1 9.8291 2.05 16.2909 12.3856 22.8229C12.5707 22.9387 12.7833 23 13 23C13.2167 23 13.4293 22.9387 13.6144 22.8229C23.95 16.2909 25 9.8291 25 7.10648C25 3.73373 22.3132 1 19 1C15.6868 1 13 4.70089 13 4.70089C13 4.70089 10.3132 1 7 1Z"
             stroke="#F59256"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </g>
         <defs>
@@ -112,9 +112,9 @@ const NoticeCategoryItem = ({
             width="34"
             height="32"
             filterUnits="userSpaceOnUse"
-            color-interpolation-filters="sRGB"
+            colorInterpolationFilters="sRGB"
           >
-            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
             <feGaussianBlur in="BackgroundImageFix" stdDeviation="2" />
             <feComposite
               in2="SourceAlpha"
@@ -220,17 +220,17 @@ const NoticeCategoryItem = ({
   );
 };
 
-NoticeCategoryItem.propTypes = {
-  _id: PropTypes.string.isRequired,
-  link: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  breed: PropTypes.string.isRequired,
-  place: PropTypes.string.isRequired,
-  age: PropTypes.string.isRequired,
-  price: PropTypes.string.isRequired,
-  page: PropTypes.string.isRequired,
-  isActive: PropTypes.bool.isRequired,
-  isFavorite: PropTypes.bool.isRequired,
-};
+// NoticeCategoryItem.propTypes = {
+//   _id: PropTypes.string.isRequired,
+//   link: PropTypes.string.isRequired,
+//   title: PropTypes.string.isRequired,
+//   breed: PropTypes.string.isRequired,
+//   place: PropTypes.string.isRequired,
+//   age: PropTypes.string.isRequired,
+//   price: PropTypes.string.isRequired,
+//   page: PropTypes.string.isRequired,
+//   isActive: PropTypes.bool.isRequired,
+//   isFavorite: PropTypes.bool.isRequired,
+// };
 
 export default NoticeCategoryItem;
