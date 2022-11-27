d78d9176b3e1703c1452c45e3111f8524ae1560e
Serhii Rybak <rybaksw@gmail.com>
2022-11-27 20:27:57 +0200

    Update isValidPetFields.js

--- backend/validators/isValidPetFields.js
+++ backend/validators/isValidPetFields.js
@@ -3,12 +3,12 @@ const Joi = require('joi');
 const addPetValidation = (req, res, next) => {
   const schema = Joi.object({
     name: Joi.string()
-      .pattern(/^([а-яё\s]+|[a-z\s]+){2,}$/iu)
+      .pattern(/^([А-Яа-яЁёЇїІіЄєҐґ'\s]+|[a-zA-Z\s]+){2,}$/)
       .max(30)
       .trim()
       .required()
       .messages({
-        'string.pattern.base': 'Name must contain only letters snd spaces',
+        'string.pattern.base': 'Name must contain only letters and spaces',
         'string.empty': 'Name is required',
         'string.max': 'Name must be less than 30 characters',
       }),
@@ -16,11 +16,11 @@ const addPetValidation = (req, res, next) => {
       'date.base': 'Birthday must be a date',
     }),
     breed: Joi.string()
-      .pattern(/^([а-яё\s]+|[a-z\s]+){2,}$/iu)
+      .pattern(/^([А-Яа-яЁёЇїІіЄєҐґ'\s]+|[a-zA-Z\s]+){2,}$/)
       .max(30)
       .trim()
       .messages({
-        'string.pattern.base': 'Breed must contain only letters snd spaces',
+        'string.pattern.base': 'Breed must contain only letters and spaces',
         'string.max': 'Breed must be less than 30 characters',
       }),
     comments: Joi.string(),
