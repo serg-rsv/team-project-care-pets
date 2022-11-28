f86c9d252dc82b41c4c0722848f370ce9b79da35
Mykola Tymoshchuk <saintnikfreak@gmail.com>
2022-11-28 02:01:27 +0200

    Create Component search form. Remake header styles. Remake home page

--- frontend/src/pages/NewsPage/NewsPage.jsx
+++ frontend/src/pages/NewsPage/NewsPage.jsx
@@ -1,22 +1,15 @@
 import s from './NewsPage.module.scss';
 // import newsData from '../../hardcodeData/news.json';
 import NewsCard from '../../components/NewsCard';
-import SearchBox from '../../components/SearchBox';
 import { useState } from 'react';
 import { useGetNewsQuery } from '../../redux/services/newsSlice';
 import { useEffect } from 'react';
+import SearchForm from '../../components/SearchForm';
 
 const NewsPage = () => {
   const [searchValue, setSearchValue] = useState('');
   const { data } = useGetNewsQuery(searchValue);
   const [news, setNews] = useState([]);
-
-  // const searchingData = query => {
-  //   const result = data.filter(
-  //     el => el.title.includes(query) || el.description.includes(query)
-  //   );
-  //   setNews(result);
-  // };
 
   useEffect(() => {
     setNews(data);
@@ -26,7 +19,7 @@ const NewsPage = () => {
     <section className="container">
       <div className={s.newsContent}>
         <h2 className={s.pageTitle}>Новини</h2>
-        <SearchBox searchQuery={setSearchValue} />
+        <SearchForm searchQuery={setSearchValue} />
         <ul className={s.newsList}>
           {news?.map((el, index) => (
             <li key={index} className={s.newsList__item}>
--- frontend/src/pages/NoticesPage/NoticesPage.jsx
+++ frontend/src/pages/NoticesPage/NoticesPage.jsx
@@ -1,21 +1,47 @@
-import { Outlet } from 'react-router-dom';
-
-import NoticesSearch from '../../components/PetPage/NoticesSearch';
-
+import SearchForm from '../../components/SearchForm';
 import NoticesCategoriesNav from '../../components/PetPage/NoticesCategoriesNav';
-
 import Container from '../../components/PetPage/NoticesCategoriesNav/Container';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useFetchNoticesMutation } from '../../redux/services/noticesSlice';
+import { useCurrentQuery } from '../../redux/services/usersSlice';
+import { markFavoriteNotice } from '../../helpers/markFavoriteNotice';
+import { setNotices, setIsLoadMore } from '../../redux/noticesSlice';
+import style from './NoticesPage.module.scss';
 
 // import s from './NoticesPage.module.scss';
 const NoticesPage = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  const [fetchNotices] = useFetchNoticesMutation();
+  const { data: user } = useCurrentQuery();
+
+  const handleSubmit = async query => {
+    const category = location.pathname.split('/').pop();
+
+    const { data } = await fetchNotices({
+      title: query,
+      category: category ? category : 'sell',
+    });
+
+    const markedNotices = markFavoriteNotice(data, user?.user?.favorites);
+
+    dispatch(setNotices(markedNotices));
+    dispatch(setIsLoadMore(false));
+  };
+
   return (
-    <div className="container">
-      <NoticesSearch />
-      <Container>
-        <NoticesCategoriesNav />
-        <Outlet />
-      </Container>
-    </div>
+    <section className="container">
+      <div className={style.noticesContent}>
+        <h2 className={style.pageTitle}>Знайдіть свого улюбленця</h2>
+        <SearchForm searchQuery={handleSubmit} />
+        <Container>
+          <NoticesCategoriesNav />
+          <Outlet />
+        </Container>
+      </div>
+    </section>
   );
 };
 
