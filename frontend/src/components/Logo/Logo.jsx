6505dffe5e10007adadc8643b159c2e5688d03fe
Serhii Rybak <rybaksw@gmail.com>
2022-12-01 22:44:47 +0200

    Update Logo.jsx
    add link to home page

--- frontend/src/components/Logo/Logo.jsx
+++ frontend/src/components/Logo/Logo.jsx
@@ -1,16 +1,16 @@
-import s from "./Logo.module.scss";
-import logo from "../../images/logo/logo.png"
-import logo2x from "../../images/logo/logo@2x.png"
+import s from './Logo.module.scss';
+import logo from '../../images/logo/logo.png';
+import logo2x from '../../images/logo/logo@2x.png';
+import { NavLink } from 'react-router-dom';
 
 const Logo = () => {
-    const logoSet = [logo, "1x", ",", logo2x, "2x"].join(" ");
+  const logoSet = [logo, '1x', ',', logo2x, '2x'].join(' ');
 
-    return (
-        <img className={s.logo}
-            srcSet={logoSet}
-            src={logo}
-            alt="logo" />
-    )
- }
+  return (
+    <NavLink to="/">
+      <img className={s.logo} srcSet={logoSet} src={logo} alt="logo" />
+    </NavLink>
+  );
+};
 
 export default Logo;
